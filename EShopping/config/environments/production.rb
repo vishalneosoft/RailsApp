6f07ae09c4d7f625e8f5529103b60938c9cfdc29
Webwerks1 <webwerks1@Z3R3P9.(none)>
2017-02-15 12:17:54 +0530

    changes in config production file

--- EShopping/config/environments/production.rb
+++ EShopping/config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+       address:              'smtp.gmail.com',
+       port:                 587,
+       domain:               'localhost:3000',
+       user_name:            'jain.vishal.neosoft@gmail.com',
+       password:             'vishal264922',
+       authentication:       'plain',
+       enable_starttls_auto: true
+    }
 end
