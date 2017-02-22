75f4ed22dc4c957ba7c20510f0186638d24027c9
Webwerks1 <webwerks1@Z3R3P9.(none)>
2017-02-22 20:51:45 +0530

    changes in the order controller

--- EShopping/app/models/order.rb
+++ EShopping/app/models/order.rb
@@ -2,14 +2,12 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :address
   has_many :order_items, dependent: :destroy
-  has_many :track_order, dependent: :destroy
+  has_many :track_orders, dependent: :destroy
   default_scope { order(:created_at => :desc) }
   has_one :used_coupon
   after_update :track_order_list
 
   def track_order_list
-    @order = Order.find(id)
-    @track_order = TrackOrder.new(status: @order.status,order_id: @order.id)
-    @track_order.save
+    self.track_orders.create(status: status)
   end
 end
